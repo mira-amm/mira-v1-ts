1a91fead207cc5b41b495e4b324c7c42ac33153f
mpoplavkov <mihail.poplavkov@gmail.com>
2024-08-31 16:28:52 +0100

    Hack for the fuels typegen

--- scripts/fetch_abis.sh
+++ scripts/fetch_abis.sh
@@ -10,6 +10,13 @@ git clone git@github.com:mira-amm/mira-v1-core.git
 git clone git@github.com:mira-amm/mira-v1-periphery.git
 
 echo "Building Mira v1 core and periphery"
+
+# Temporary hack for the fuels typegen to work, while it doesn't support PoolId custom type
+# See https://forum.fuel.network/t/typescript-typegen-doesnt-support-some-types/6546/6
+find . -type f -exec perl -pi -e 's/: PoolId/: (AssetId, AssetId, bool)/g' {} +
+find . -type f -exec perl -pi -e 's/<PoolId/<(AssetId, AssetId, bool)/g' {} +
+find . -type f -exec perl -pi -e 's/-> PoolId/-> (AssetId, AssetId, bool)/g' {} +
+
 cd mira-v1-core
 forc build --release
 cd ../mira-v1-periphery
@@ -32,3 +39,5 @@ mv -f tmp_abis/mira-v1-periphery/scripts/swap_exact_input_script/out/release/ sw
 mv -f tmp_abis/mira-v1-periphery/scripts/swap_exact_output_script/out/release/ sway_abis/swap_exact_output_script
 
 rm -rf tmp_abis
+
+pnpm fuels typegen -i sway_abis/*/release/*-abi.json -o ./src/sdk/typegen
